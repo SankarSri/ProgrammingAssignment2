b3bb4c05c032305baac6c4ea2d183de55026b8a8
SankarSri <Sankar.Manepalli@ArtOfLiving.Org>
2014-07-27 20:04:31 +0530

    Added the function bodies per assignment instructions

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invMatrix <- NULL
+  setCacheMatrix <- function(y)
+  {
+    x <<- y
+    invMatrix <<- NULL
+  }
+  getCacheMatrix <- function() x
+  setInvCacheMatrix <- function(toInvM) invMatrix <<- toInvM
+  getInvCacheMatrix <- function() invMatrix
+  list(setCacheMatrix = setCacheMatrix,
+       getCacheMatrix = getCacheMatrix,
+       setInvCacheMatrix = setInvCacheMatrix,
+       getInvCacheMatrix = getInvCacheMatrix)
 }
 
 
@@ -12,4 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invMat <- x$getInvCacheMatrix()
+  if (!is.null(invMat))
+  {
+    message("Getting cached data")
+    return(invMat)
+  }
+  origMat <- x$getCacheMatrix()
+  invMat <- solve(origMat,...)
+  x$setInvCacheMatrix(invMat)
+  invMat
 }
